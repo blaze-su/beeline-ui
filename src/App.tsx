91c7c6468241208a0c69c36b6474cb27b3fd53c1
Aleksandr Sidorov <a.sidorov@blaze.su>
2021-09-09 01:38:27 +0300

    feat(client): no-cors

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+const checkAvailableNumber = (cityCode = "moskva", favoriteNumber = "4334445") => {
+	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const numberUrl = `https://${cityCode}.beeline.ru/fancynumber/favourite/?number=${favoriteNumber}`;
+        console.log(cityCode);
+
+    return axios.get(proxyUrl+numberUrl).then((res) => {
+        res.data.numbers.forEach((category: any) => {
+            category.numbers.forEach(({value}: any) => {
+                if(value.includes(favoriteNumber)) {
+                    console.log(cityCode, ":: found ", value)
+                }
+            })
+        })
+    }).catch((err) => {
+		console.log(err, cityCode)
+	})
+}
+
+const App = () => {
+	const [cities, setCities] = useState<any[]>([]);	
+
+  useEffect(() => {
+    const fetchCities = async () => { 
+		const data = new Map<string, any>()  
+		const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+		const regionsUrl = 'https://moskva.beeline.ru/region/regionsList/?ui-culture=ru-ru';
+
+		await axios.post(proxyUrl+regionsUrl, { clearJson: "true",  })
+			.then((res) => {
+				return res.data.allRegionGroups;
+			})
+			.then((regionGroups) => {
+				regionGroups.forEach((region: any) => {
+					region.forEach(({ items }: any) => {
+						items.forEach((city: any) => {
+							data.set(city.code, city)
+						})
+					}) 
+				});
+			})
+
+		setCities(Array.from(data.values()))
+
+		for (let city of cities) {
+			await checkAvailableNumber(city.code)
+		}
+	}
+
+	fetchCities();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		{cities.map(({code}) => <div key={code}>{code}</div>)}
+	</div>
   );
 }
 
